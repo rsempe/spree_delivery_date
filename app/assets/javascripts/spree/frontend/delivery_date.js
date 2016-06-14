20d3a2539ccf28a5fc99024f613705e74e6cae36
Romain Sempe <romain.sempe@gmail.com>
2016-06-14 12:21:11 +0200

    Deactivate Sunday on Calendar

--- app/assets/javascripts/spree/frontend/delivery_date.js
+++ app/assets/javascripts/spree/frontend/delivery_date.js
@@ -2,10 +2,14 @@ function SpreeDeliveryDate() {
   var that = this;
 
   this.initializeDatePicker = function() {
-     $('#order_delivery_date').datepicker({
-       dateFormat: "dd/mm/yy",
-       minDate: 1
-     });
+    $('#order_delivery_date').datepicker({
+      dateFormat: "dd/mm/yy",
+      minDate: 1,
+      beforeShowDay: function(date) {
+        var day = date.getDay();
+        return [(day != 0), ''];
+      }
+    });
   };
 }
 
